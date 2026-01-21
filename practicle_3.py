42744339a4797e73aaf1c4a08614a7e144b56dea
Aditya Thakre <12345678+Aditya823-stark@users.noreply.github.com>
2026-01-21 11:44:01 +0530

    initial commit

---
+++ practicle_3.py
@@ -0,0 +1,36 @@
+
+# 1
+# 2 3
+# 3 4 5
+# 4 5 6 7
+# 5 6 7 8 9
+# for i in range(1, 6):
+#     for j in range(1,i+1):
+#         print(i, end=" ")
+#         i=i+1
+#     print()
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# for i in range(1, 6):
+#     for j in range(1, i +1):
+#         print("*", end=" ")
+#     print()
+
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j, end=" ")
+#     print()
+
+
+
+
+
